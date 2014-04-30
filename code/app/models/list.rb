e97549b2795d923165529dd6fb04c9a664e38cac
Theneva <splugs@gmail.com>
2014-04-30 21:29:54 +0200

    Lists display list items

--- code/app/models/list.rb
+++ code/app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ActiveRecord::Base
   has_many :list_items, dependent: :destroy
+
+  def list_items
+    ListItem.where list_id: self.id
+  end
 end
--- code/app/models/list_item.rb
+++ code/app/models/list_item.rb
@@ -1,3 +1,7 @@
 class ListItem < ActiveRecord::Base
   belongs_to :list
+
+  def to_s
+    "List item ##{self.id}: #{label}"
+  end
 end
